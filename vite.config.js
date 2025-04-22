7920a92c4438338c1ff1cc2b26d028172af49053
Felipe Miranda de Lima <fmlima4@outlook.com>
2025-04-22 20:11:44 -0300

    fix api endpoint

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/palavras': {
-        target: 'http://localhost:3000',
+        target: 'https://random-word-api-togh.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
